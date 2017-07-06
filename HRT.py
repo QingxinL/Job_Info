ce7bc2b5a6d0d0fa12b1ef064d88e5a07f965c9b
Qingxin Li <qli1998@uw.edu>
2017-07-06 18:15:05 +0800

    Use url inside the iframe

--- HRT.py
+++ HRT.py
@@ -9,7 +9,19 @@ HRT = db['Hudson_River_Trading_Collection']
 
 HRT_dict = {}
 
-html = urlopen('http://www.hudson-trading.com/careers/').read()
-soup = BeautifulSoup(html,'lxml')
+# TODO: this url is the html within the iframe - didn't figure out how to get this link from the origin page
+def job_Scraping(url):
+    html = urlopen(url)
+    bsObj = BeautifulSoup(html,'lxml')
+    jobs = bsObj.findAll('a',{'data-mapped':'true'})
+    for job in jobs:
+        HRT_dict['Title']=job.text
+        HRT_dict['Website']=job.get('href')
+    locations = bsObj.findAll('span',class_='location')
+    for loc in locations:
+        HRT_dict['Location']=loc.text
+    print(HRT_dict)
+    HRT.update(spec=HRT_dict,document=HRT_dict,upsert=True)
+    print('End of Program')
 
-# Need to deal with iframe
+job_Scraping('https://boards.greenhouse.io/embed/job_board?for=wehrtyou&b=http://www.hudson-trading.com/careers/')
