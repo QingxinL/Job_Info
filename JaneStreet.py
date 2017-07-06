3b5ca8ba3faf08f60c2ca07df11395cd438a49e5
Qingxin Li <qli1998@uw.edu>
2017-07-06 16:07:09 +0800

    Might need JavaScript to control display property

--- JaneStreet.py
+++ JaneStreet.py
@@ -12,7 +12,16 @@ JSC_dict = {}
 
 # https://www.janestreet.com/join-jane-street/open-positions/
 # TODO: Need Selenium WebDriver - for the 'View Open Positions' button on the web
-
+# Selenium does not interact with elements that have "display:none"
+# Need execute_script() to set the value of the select - JavaScript
+'''    
+try:
+    selenium.webdriver.support.ui.WebDriverWait(driver, 60).until(EC.visibility_of_element_located((By.ID, 'some_other_id_on_page')))
+    selenium.execute_script("document.getElementById('some_id').style.display='inline-block';")
+    element = selenium.webdriver.support.ui.WebDriverWait(driver, 60).until(EC.visibility_of_element_located((By.ID, 'some_id')))
+    selenium.webdriver.support.ui.Select(element).select_by_value('1')
+    except Exception as ex:
+    print(ex)'''
 
 
 #job_Scraping('https://www.janestreet.com/join-jane-street/open-positions/')
